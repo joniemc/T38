717d6999e242264bbcd224052a2ec3003a0054ca
gabmen27 <gabrieljesus680@gmail.com>
2026-02-20 19:56:03 -0600

    Copiando el codigo de mi examen. Lastimosamente no lei bien el examen y no entendi bien el primer requisito del ejercicio que es que vaya todos los datos.

--- PautaExamenI/app.js
+++ PautaExamenI/app.js
@@ -1,34 +1,48 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
 
-const libros = [{"id":1,"titulo":"La comunidad del anillo","autor":"Jrr tolkien","anioPublicacion":1955,"estado":"Disponible"}]
+let libros = [{id:1,titulo:'Cien años de soledad',autor:'Gabriel García',anioPublicacion:1967,estado:'disponible'},
+    {id:2,titulo:'El resplandor',autor:'Stephen King',anioPublicacion:1977,estado:'reservado'},
+    {id:3,titulo:'El principito',autor:'Antoine de Saint-Exupéry',anioPublicacion:1977,estado:'Reservado'},
+    {id:4,titulo:'El Túnel',autor:'Ernesto Sabaro',anioPublicacion:1948,estado:'Disponible'}];
 
-app.get("/libros",(req,res)=>{
-    res.json({status:200,message:'Success',data:libros});
+app.get('/libros',(req,res)=>{
+    res.status(200).json({status:200,message:'Sucess',data:libros});
+})    
 
-});
-
-app.post("/libros",(req,res)=>{
-const{titulo,autor,anioPublicacion,estado} = req.body;
-
-if( titulo && autor && anioPublicacion && estado){
-    const nuevoLibro = {
-        id: libros.length + 1,
-        titulo,
-        autor,
-        anioPublicacion,
-        estado
-     }
-}
-
-libros.push(nuevoLibro);
-res.json({status:201,message:'Libro agregado',data:nuevoLibro});
+app.post('/libros',(req,res)=>{
+    const libro = req.body;
+    
+    libros.push(libro);
+    res.status(201).json({status:201,message:'Libro agregado exitosamente...'});
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Escuchando en http://localhost:${PORT}/`);
+app.put('/libros/:id',(req,res)=>{
+    const libro = req.body;
+    const id = parseInt(req.params.id);
+
+    let isExist = false;
+
+    libros.forEach(book =>{
+        if(book.id === id){
+            isExist = true;
+            book.titulo = libro.titulo;
+            book.autor = libro.autor;
+            book.anioPublicacion = libro.anioPublicacion;
+            book.estado = libro.estado;
+        }
+    })
+
+    if(isExist){
+        return res.status(200).json({status:200, message:'Libro Actualizado exitosamente...'});
+    }else{
+        return res.status(404).json({status:404,message:'Libro no encontrado...'});
+    }
+
 });
+app.listen(PORT, ()=>{
+    console.log(`El servidor esta escuchando en http://localhost:${PORT}`);
+})
